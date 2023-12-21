cef1d6b4ef8c576cd9d79abd7b34936e64222a20
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-12-21 10:27:51 +0530

    sideNavBar hamburger  button

--- components/SideNavBar.js
+++ components/SideNavBar.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Disclosure } from '@headlessui/react';
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 
 function SideNavBar() {
   return (
-    <div>
-        <h1>SideNavBar</h1>
-    </div>
+    <Disclosure as="nav">
+        <Disclosure.Button className="flex items-center justify-between w-full px-2 py-1 text-sm font-medium text-gray-500  border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <GiHamburgerMenu className="w-6 h-6" 
+                aria-hidden="true"
+            />
+        </Disclosure.Button>
+    </Disclosure>
   )
 }
 
