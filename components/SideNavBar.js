99b95276ba88a1a998e16b002d5105c990e564ae
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-12-21 10:44:11 +0530

    hover background changes

--- components/SideNavBar.js
+++ components/SideNavBar.js
@@ -6,7 +6,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 function SideNavBar() {
   return (
     <Disclosure as="nav">
-        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-900 hover:text-white hover:bg-black ">
+        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-900 hover:text-white hover:bg-gray-800 ">
             <GiHamburgerMenu className="w-6 h-6" 
                 aria-hidden="true"
             />
