0c1013337887d54bd3678c9eeeb8cfcb617c3b54
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-12-21 10:39:08 +0530

    hamburger design changes

--- components/SideNavBar.js
+++ components/SideNavBar.js
@@ -6,7 +6,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 function SideNavBar() {
   return (
     <Disclosure as="nav">
-        <Disclosure.Button className="flex items-center justify-between w-full px-2 py-1 text-sm font-medium text-gray-500  border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-900 hover:text-white hover:bg-black ">
             <GiHamburgerMenu className="w-6 h-6" 
                 aria-hidden="true"
             />
