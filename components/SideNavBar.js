ce266d9a7a1de655f6866613f84375318b1ccc8e
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-12-25 09:38:22 +0530

    side nav 25

--- components/SideNavBar.js
+++ components/SideNavBar.js
@@ -5,13 +5,20 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 
 function SideNavBar() {
   return (
-    <Disclosure as="nav">
-        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-900 hover:text-white hover:bg-gray-800 ">
-            <GiHamburgerMenu className="w-6 h-6" 
+    <div>
+        <Disclosure as="nav">
+        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-900 hover:text-white hover:bg-gray-900 ">
+            <GiHamburgerMenu className="block md:hidden w-6 h-6"
                 aria-hidden="true"
             />
         </Disclosure.Button>
+
+        <div className='p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:w-60 lg:left-0 peer focus:left-0 peer:transition ease-out delay-150 duration-200'>
+               
+        </div>
+        
     </Disclosure>
+    </div>
   )
 }
 
